44522ec32e3298ddddb677d4041db59d9610ac86
zehancoder <zehanjh93@gmail.com>
2026-04-05 11:22:23 +0600

    ip access and api links chaneges

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://moodify-music-player.onrender.com/",
     credentials: true,
   }),
 );
