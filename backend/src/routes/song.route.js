79763d014b8641b0372c2362a80968df5b413231
Jonaeed Hossen <zehanjh93@gmail.com>
2026-04-16 00:20:27 +0600

    updates

--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -5,5 +5,5 @@ const identifyUser = require("../middleware/auth.middleware");
 const songRouter = express.Router();
 songRouter.post("/create/song", upload.single("song"), uploadSong.uploadSong);
 songRouter.get("/get/songs", uploadSong.getSongByMood);
-songRouter.get("/getAll/songs", identifyUser, uploadSong.getAllsongContoller);
+songRouter.get("/getAll/songs", uploadSong.getAllsongContoller);
 module.exports = songRouter;
